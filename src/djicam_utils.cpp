465beac4a69265c11a451e188bd6fe9e14e3794f
LIUTianbo@uav4 <liutianbocn@gmail.com>
2016-01-16 09:07:59 +0000

    format

--- src/djicam_utils.cpp
+++ src/djicam_utils.cpp
@@ -1,103 +1,97 @@
 #include "djicam_utils.h"
 #include <string.h>
 
-sRGB yuvTorgb(int Y, int U, int V)
-{
-	sRGB rgb;
-	rgb.r = (int)(Y + 1.4075 * (V-128));
-	rgb.g = (int)(Y - 0.3455 * (U-128) - 0.7169*(V-128));
-	rgb.b = (int)(Y + 1.779 * (U-128));
-	rgb.r =(rgb.r<0? 0: rgb.r>255? 255 : rgb.r);
-	rgb.g =(rgb.g<0? 0: rgb.g>255? 255 : rgb.g);
-	rgb.b =(rgb.b<0? 0: rgb.b>255? 255 : rgb.b);
-	return rgb;
+sRGB yuvTorgb(int Y, int U, int V) {
+    sRGB rgb;
+    rgb.r = (int)(Y + 1.4075 * (V - 128));
+    rgb.g = (int)(Y - 0.3455 * (U - 128) - 0.7169 * (V - 128));
+    rgb.b = (int)(Y + 1.779 * (U - 128));
+    rgb.r = (rgb.r < 0 ? 0 : rgb.r > 255 ? 255 : rgb.r);
+    rgb.g = (rgb.g < 0 ? 0 : rgb.g > 255 ? 255 : rgb.g);
+    rgb.b = (rgb.b < 0 ? 0 : rgb.b > 255 ? 255 : rgb.b);
+    return rgb;
 }
 
-unsigned char * NV12ToRGB(unsigned char * src, unsigned char * rgb, int width, int height){
-	int numOfPixel = width * height;
-	int positionOfU = numOfPixel;
-	int startY,step,startU,Y,U,V,index,nTmp;
-	sRGB tmp;
+unsigned char* NV12ToRGB(unsigned char* src, unsigned char* rgb, int width,
+                         int height) {
+    int numOfPixel = width * height;
+    int positionOfU = numOfPixel;
+    int startY, step, startU, Y, U, V, index, nTmp;
+    sRGB tmp;
 
-	for(int i=0; i<height; i++){
-		startY = i*width;
-		step = i/2*width;
-		startU = positionOfU + step;
-		for(int j = 0; j < width; j++){
-			Y = startY + j;
-			if(j%2 == 0)
-				nTmp = j;
-			else
-				nTmp = j - 1;
-			U = startU + nTmp;
-			V = U + 1;
-			index = Y*3;
-			tmp = yuvTorgb((int)src[Y], (int)src[U], (int)src[V]);
-			rgb[index+0] = (char)tmp.b;
-			rgb[index+1] = (char)tmp.g;
-			rgb[index+2] = (char)tmp.r;
-		}
-	}
-	return rgb;
+    for (int i = 0; i < height; i++) {
+        startY = i * width;
+        step = i / 2 * width;
+        startU = positionOfU + step;
+        for (int j = 0; j < width; j++) {
+            Y = startY + j;
+            if (j % 2 == 0)
+                nTmp = j;
+            else
+                nTmp = j - 1;
+            U = startU + nTmp;
+            V = U + 1;
+            index = Y * 3;
+            tmp = yuvTorgb((int)src[Y], (int)src[U], (int)src[V]);
+            rgb[index + 0] = (char)tmp.b;
+            rgb[index + 1] = (char)tmp.g;
+            rgb[index + 2] = (char)tmp.r;
+        }
+    }
+    return rgb;
 }
 
-bool YUV420_To_BGR24(unsigned char *puc_y, unsigned char *puc_u, unsigned char *puc_v, unsigned char *puc_rgb, int width_y, int height_y)
-{
-	if (!puc_y || !puc_u || !puc_v || !puc_rgb)
-	{
-		return false;
-	}
-	int baseSize = width_y * height_y;
-	int rgbSize = baseSize * 3;
+bool YUV420_To_BGR24(unsigned char* puc_y, unsigned char* puc_u,
+                     unsigned char* puc_v, unsigned char* puc_rgb, int width_y,
+                     int height_y) {
+    if (!puc_y || !puc_u || !puc_v || !puc_rgb) {
+        return false;
+    }
+    int baseSize = width_y * height_y;
+    int rgbSize = baseSize * 3;
 
-	BYTE* rgbData = new BYTE[rgbSize];
-	memset(rgbData, 0, rgbSize);
+    BYTE* rgbData = new BYTE[rgbSize];
+    memset(rgbData, 0, rgbSize);
 
-	int temp = 0;
+    int temp = 0;
 
-	BYTE* rData = rgbData; 
-	BYTE* gData = rgbData + baseSize;
-	BYTE* bData = gData + baseSize;
+    BYTE* rData = rgbData;
+    BYTE* gData = rgbData + baseSize;
+    BYTE* bData = gData + baseSize;
 
-	int uvIndex =0, yIndex =0;
+    int uvIndex = 0, yIndex = 0;
 
+    for (int y = 0; y < height_y; y++) {
+        for (int x = 0; x < width_y; x++) {
+            uvIndex = (y >> 1) * (width_y >> 1) + (x >> 1);
+            yIndex = y * width_y + x;
 
-	for(int y=0; y < height_y; y++)
-	{
-		for(int x=0; x < width_y; x++)
-		{
-			uvIndex = (y>>1) * (width_y>>1) + (x>>1);
-			yIndex = y * width_y + x;
+            temp = (int)(puc_y[yIndex] + (puc_v[uvIndex] - 128) * 1.4022);
+            rData[yIndex] = temp < 0 ? 0 : (temp > 255 ? 255 : temp);
 
-			temp = (int)(puc_y[yIndex] + (puc_v[uvIndex] - 128) * 1.4022);
-			rData[yIndex] = temp<0 ? 0 : (temp > 255 ? 255 : temp);
+            temp = (int)(puc_y[yIndex] + (puc_u[uvIndex] - 128) * (-0.3456) +
+                         (puc_v[uvIndex] - 128) * (-0.7145));
+            gData[yIndex] = temp < 0 ? 0 : (temp > 255 ? 255 : temp);
 
-			temp = (int)(puc_y[yIndex] + (puc_u[uvIndex] - 128) * (-0.3456) +
-					(puc_v[uvIndex] - 128) * (-0.7145));
-			gData[yIndex] = temp < 0 ? 0 : (temp > 255 ? 255 : temp);
+            temp = (int)(puc_y[yIndex] + (puc_u[uvIndex] - 128) * 1.771);
+            bData[yIndex] = temp < 0 ? 0 : (temp > 255 ? 255 : temp);
+        }
+    }
 
-			temp = (int)(puc_y[yIndex] + (puc_u[uvIndex] - 128) * 1.771);
-			bData[yIndex] = temp < 0 ? 0 : (temp > 255 ? 255 : temp);
-		}
-	}
+    int widthStep = width_y * 3;
+    for (int y = 0; y < height_y; y++) {
+        for (int x = 0; x < width_y; x++) {
+            puc_rgb[y * widthStep + x * 3 + 2] = rData[y * width_y + x];  // R
+            puc_rgb[y * widthStep + x * 3 + 1] = gData[y * width_y + x];  // G
+            puc_rgb[y * widthStep + x * 3 + 0] = bData[y * width_y + x];  // B
+        }
+    }
 
-	int widthStep = width_y*3;
-	for (int y = 0; y < height_y; y++)
-	{
-		for (int x = 0; x < width_y; x++)
-		{
-			puc_rgb[y * widthStep + x * 3 + 2] = rData[y * width_y + x]; //R
-			puc_rgb[y * widthStep + x * 3 + 1] = gData[y * width_y + x]; //G
-			puc_rgb[y * widthStep + x * 3 + 0] = bData[y * width_y + x]; //B
-		}
-	}
-
-	if (!puc_rgb)
-	{
-		return false;
-	}
-	delete [] rgbData;
-	return true;
+    if (!puc_rgb) {
+        return false;
+    }
+    delete[] rgbData;
+    return true;
 }
 
 // IplImage* YUV420_To_IplImage(unsigned char* pYUV420, int width, int height)
@@ -114,11 +108,12 @@ bool YUV420_To_BGR24(unsigned char *puc_y, unsigned char *puc_u, unsigned char *
 
 // 	int temp = 0;
 
-// 	BYTE* yData = pYUV420; 
-// 	BYTE* uData = pYUV420 + baseSize; 
-// 	BYTE* vData = uData + (baseSize>>2); 
+// 	BYTE* yData = pYUV420;
+// 	BYTE* uData = pYUV420 + baseSize;
+// 	BYTE* vData = uData + (baseSize>>2);
 
-// 	if(YUV420_To_BGR24(yData, uData, vData, pRGB24, width, height) == false || !pRGB24)
+// 	if(YUV420_To_BGR24(yData, uData, vData, pRGB24, width, height) == false
+// || !pRGB24)
 // 	{
 // 		return NULL;
 // 	}
--- src/m100_x3_node.cpp
+++ src/m100_x3_node.cpp
@@ -10,156 +10,151 @@
 
 #define IMAGE_W 1280
 #define IMAGE_H 720
-#define FRAME_SIZE (IMAGE_W*IMAGE_H*3)
+#define FRAME_SIZE (IMAGE_W * IMAGE_H * 3)
 
 #define mode (GETBUFFER_MODE | TRANSFER_MODE)
 
-void fill_camera_info_message_with_default_value(sensor_msgs::CameraInfo& cam_info) {
-	cam_info.header.frame_id = "/camera";
-	cam_info.distortion_model = "";
-	cam_info.D = {-0.1297646493949856, 0.0946885697670611, -0.0002935002712265514, -0.00022663675362156343, 0.0};
-	cam_info.K = {388.40923066779754, 0.0, 318.06257844065226, 0.0, 518.1538449374815, 241.17339016626644, 0.0, 0.0, 1.0};
-	cam_info.R = {1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0};
-	cam_info.P = {373.5429992675781, 0.0, 317.51131336952494, 0.0, 0.0, 504.4360656738281, 240.6131009245937, 0.0, 0.0, 0.0, 1.0, 0.0};
-	cam_info.binning_x = 0;
-	cam_info.binning_x = 0;
+void fill_camera_info_message_with_default_value(
+    sensor_msgs::CameraInfo& cam_info) {
+    cam_info.header.frame_id = "/camera";
+    cam_info.distortion_model = "";
+    cam_info.D = {-0.1297646493949856, 0.0946885697670611,
+                  -0.0002935002712265514, -0.00022663675362156343, 0.0};
+    cam_info.K = {388.40923066779754, 0.0, 318.06257844065226, 0.0,
+                  518.1538449374815, 241.17339016626644, 0.0, 0.0, 1.0};
+    cam_info.R = {1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0};
+    cam_info.P = {373.5429992675781, 0.0, 317.51131336952494, 0.0, 0.0,
+                  504.4360656738281, 240.6131009245937, 0.0, 0.0, 0.0, 1.0,
+                  0.0};
+    cam_info.binning_x = 0;
+    cam_info.binning_x = 0;
 
-	cam_info.roi.x_offset = 0;
-	cam_info.roi.y_offset = 0;
-	cam_info.roi.height = 0;
-	cam_info.roi.width = 0;
-	cam_info.roi.do_rectify = false;
+    cam_info.roi.x_offset = 0;
+    cam_info.roi.y_offset = 0;
+    cam_info.roi.height = 0;
+    cam_info.roi.width = 0;
+    cam_info.roi.do_rectify = false;
 }
 
-int main(int argc, char **argv)
-{
-	ros::init(argc,argv,"m100_x3");
-	ros::NodeHandle nh("~");
-	
-	image_transport::ImageTransport transport(nh);
-	image_transport::Publisher image_pub;
-	ros::Publisher caminfo_pub;
-	sensor_msgs::Image im;
-	sensor_msgs::CameraInfo cam_info;
-	cv_bridge::CvImage cvi;
-	
-	IplImage * pRawImg;
-	IplImage * pImg;
-	unsigned char * pData;
-	
-	int dest_image_w, dest_image_h;
-	int ret;
-	unsigned char buffer[FRAME_SIZE] = {0};
-	unsigned int nframe = 0;
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "m100_x3");
+    ros::NodeHandle nh("~");
 
-	bool enable_color = false;
-	bool enable_camera_info = false;
-	int downsample_factor = 0;
-	
-	nh.param("enable_color", enable_color, false);
-	nh.param("downsample_factor", downsample_factor, 2);
-	nh.param("camera_info", enable_camera_info, false);
+    image_transport::ImageTransport transport(nh);
+    image_transport::Publisher image_pub;
+    ros::Publisher caminfo_pub;
+    sensor_msgs::Image im;
+    sensor_msgs::CameraInfo cam_info;
+    cv_bridge::CvImage cvi;
 
-	ros::Time now_time = ros::Time::now();
-	cvi.header.frame_id = "image";
-	cvi.encoding = enable_color? "bgr8":"mono8";
+    IplImage* pRawImg;
+    IplImage* pImg;
+    unsigned char* pData;
 
-	if (downsample_factor > 1) {
-		dest_image_w = IMAGE_W / downsample_factor;
-		dest_image_h = IMAGE_H / downsample_factor;
-	}
-	else {
-		dest_image_w = IMAGE_W;
-		dest_image_h = IMAGE_H;
-	}
+    int dest_image_w, dest_image_h;
+    int ret;
+    unsigned char buffer[FRAME_SIZE] = {0};
+    unsigned int nframe = 0;
 
-	if (enable_color) {
-		pRawImg = cvCreateImage(cvSize(IMAGE_W, IMAGE_H), IPL_DEPTH_8U, 3);
-		pImg = cvCreateImage(cvSize(dest_image_w, dest_image_h), IPL_DEPTH_8U, 3);
-		pData = new unsigned char[IMAGE_W * IMAGE_H * 3];
-	}
-	else {
-		pRawImg = cvCreateImage(cvSize(IMAGE_W, IMAGE_H), IPL_DEPTH_8U, 1);
-		pImg = cvCreateImage(cvSize(dest_image_w, dest_image_h), IPL_DEPTH_8U, 1);
-	}
+    bool enable_color = false;
+    bool enable_camera_info = false;
+    int downsample_factor = 0;
 
-	image_pub = transport.advertise("image_raw", 1);
-	if(enable_camera_info) {
-		caminfo_pub = nh.advertise<sensor_msgs::CameraInfo>("dji_sdk/camera_info",1);
-		cam_info.width = dest_image_w;
-		cam_info.height = dest_image_h;
-		fill_camera_info_message_with_default_value(cam_info);
-	};
-	
-	ROS_INFO("[M100-X3] ImageType: %s | ImageSize: %dx%d | CameraInfo: %s",\
-		enable_color? "BGR":"Grayscale",
-		dest_image_w, dest_image_h,
-		enable_camera_info? "ON":"OFF");
-	
-	ROS_INFO("[M100-X3] Start X3 initialization.");
-	ret = manifold_cam_init(mode);
-	if(ret == -1)
-	{
-		ROS_ERROR("[M100-X3] X3 init error. \n");
-		return -1;
-	}
-    else {
+    nh.param("enable_color", enable_color, false);
+    nh.param("downsample_factor", downsample_factor, 2);
+    nh.param("camera_info", enable_camera_info, false);
+
+    ros::Time now_time = ros::Time::now();
+    cvi.header.frame_id = "image";
+    cvi.encoding = enable_color ? "bgr8" : "mono8";
+
+    if (downsample_factor > 1) {
+        dest_image_w = IMAGE_W / downsample_factor;
+        dest_image_h = IMAGE_H / downsample_factor;
+    } else {
+        dest_image_w = IMAGE_W;
+        dest_image_h = IMAGE_H;
+    }
+
+    if (enable_color) {
+        pRawImg = cvCreateImage(cvSize(IMAGE_W, IMAGE_H), IPL_DEPTH_8U, 3);
+        pImg =
+            cvCreateImage(cvSize(dest_image_w, dest_image_h), IPL_DEPTH_8U, 3);
+        pData = new unsigned char[IMAGE_W * IMAGE_H * 3];
+    } else {
+        pRawImg = cvCreateImage(cvSize(IMAGE_W, IMAGE_H), IPL_DEPTH_8U, 1);
+        pImg =
+            cvCreateImage(cvSize(dest_image_w, dest_image_h), IPL_DEPTH_8U, 1);
+    }
+
+    image_pub = transport.advertise("image_raw", 1);
+    if (enable_camera_info) {
+        caminfo_pub =
+            nh.advertise<sensor_msgs::CameraInfo>("dji_sdk/camera_info", 1);
+        cam_info.width = dest_image_w;
+        cam_info.height = dest_image_h;
+        fill_camera_info_message_with_default_value(cam_info);
+    };
+
+    ROS_INFO("[M100-X3] ImageType: %s | ImageSize: %dx%d | CameraInfo: %s",
+             enable_color ? "BGR" : "Grayscale", dest_image_w, dest_image_h,
+             enable_camera_info ? "ON" : "OFF");
+
+    ROS_INFO("[M100-X3] Start X3 initialization.");
+    ret = manifold_cam_init(mode);
+    if (ret == -1) {
+        ROS_ERROR("[M100-X3] X3 init error. \n");
+        return -1;
+    } else {
         ROS_INFO("[M100-X3] X3 init success!\n");
     }
 
     ros::Rate r(200.0);
-	while(ros::ok())
-	{
-		r.sleep();
-		now_time = ros::Time::now();
-		ret = manifold_cam_read(buffer, &nframe, CAM_NON_BLOCK);
+    while (ros::ok()) {
+        r.sleep();
+        now_time = ros::Time::now();
+        ret = manifold_cam_read(buffer, &nframe, CAM_NON_BLOCK);
         ROS_DEBUG("[M100-X3] manifold_cam_read(): ret=%d", ret);
-		if(ret == 0) {
-			// pass
-		}
-		else if (ret == -1) {
-			break;
-		}
-		else if (ret == (FRAME_SIZE/2)) {
-			// (FRAME_SIZE/2) == 1382400, which is the expect ret value
+        if (ret == 0) {
+            // pass
+        } else if (ret == -1) {
+            break;
+        } else if (ret == (FRAME_SIZE / 2)) {
+            // (FRAME_SIZE/2) == 1382400, which is the expect ret value
             if (enable_color) {
-				NV12ToRGB(buffer,pData,1280,720);
-				memcpy(pRawImg->imageData,pData,FRAME_SIZE);
-            	
-            }
-            else {
-				memcpy(pRawImg->imageData,buffer,FRAME_SIZE/3);
+                NV12ToRGB(buffer, pData, 1280, 720);
+                memcpy(pRawImg->imageData, pData, FRAME_SIZE);
+
+            } else {
+                memcpy(pRawImg->imageData, buffer, FRAME_SIZE / 3);
             }
 
             if (IMAGE_W == dest_image_w && IMAGE_H == dest_image_h) {
-            	// pass
+                cvi.image = pRawImg;
+            } else {
+                cvResize(pRawImg, pImg, CV_INTER_LINEAR);
+                cvi.image = pImg;
             }
-            else {
-				cvResize(pRawImg,pImg,CV_INTER_LINEAR);
+
+            cvi.header.stamp = now_time;
+            cvi.toImageMsg(im);
+            image_pub.publish(im);
+
+            if (enable_camera_info) {
+                cam_info.header.stamp = now_time;
+                caminfo_pub.publish(cam_info);
             }
+        } else {
+            ROS_WARN("[M100-X3] manifold_cam_read(): ret=%d", ret);
+        }
+    }
 
-			cvi.header.stamp = now_time;
-			cvi.image = pImg;
-			cvi.toImageMsg(im);
-			image_pub.publish(im);
+    while (!manifold_cam_exit()) {
+        ROS_WARN("[M100-X3] X3 failed to exit, retrying...");
+        sleep(1);
+    }
 
-			if (enable_camera_info) {
-				cam_info.header.stamp = now_time;
-				caminfo_pub.publish(cam_info);
-			}
-		}
-		else {
-			ROS_WARN("[M100-X3] manifold_cam_read(): ret=%d", ret);
-		}
-	}
-
-	while(!manifold_cam_exit())
-	{
-		ROS_WARN("[M100-X3] X3 failed to exit, retrying...");
-		sleep(1);
-	}
-
-	ROS_INFO("[M100-X3] Exit...");
-	sleep(1);
-	return 0;
+    ROS_INFO("[M100-X3] Exit...");
+    sleep(1);
+    return 0;
 }
