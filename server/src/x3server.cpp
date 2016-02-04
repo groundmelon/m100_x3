594f82371494edb2a17b1f7f9960b83979ec9618
LIUTianbo@uav3 <liutianbocn@gmail.com>
2016-02-04 07:39:11 +0000

    before

--- server/src/x3server.cpp
+++ server/src/x3server.cpp
@@ -17,12 +17,17 @@ const size_t FRAME_SIZE = 1280*720*3;
 
 using boost::asio::ip::udp;
 using boost::asio::ip::tcp;
+using std::cout;
+using std::cerr;
+using std::endl;
 
 typedef boost::asio::detail::socket_option::integer<SOL_SOCKET, SO_SNDBUF> send_buf_opt_t;
 
 uint8_t imgbuf[FRAME_SIZE];
 uint8_t sockbuf[TOTAL_LEN];
 bool accepted;
+
+
 
 std::string make_daytime_string() {
     using namespace std;  // For time_t, time and ctime;
@@ -36,7 +41,7 @@ void accept_handle(const boost::system::error_code& error) {
     if (!error) 
         accepted = true;
     else
-        (*pOS) << "accept handle error=" << error.message() << std::endl;
+        cout << "accept handle error=" << error.message() << endl;
 }
 
 void handle_wait(const boost::system::error_code& error,  
@@ -44,40 +49,34 @@ void handle_wait(const boost::system::error_code& error,
     if (!error)
         io.stop();
     else
-        std::cout << "handle_wait error="<< error.message() << std::endl;
+        cout << "handle_wait error="<< error.message() << endl;
 }
 
 int main() {
-    std::ofstream logstream("/tmp/x3server.log");
-    if (!logstream.is_open()) {
-        return EX_CANTCREAT;
-    }
-
-    pOS = &logstream;
-    logstream << "x3server starts at " << make_daytime_string() << std::endl;
+    cout << "x3server starts at " << make_daytime_string() << endl;
 
     int cam_func_ret;
     size_t cam_nframe = 0;
     bool cam_is_on;
     accepted = false;
-    logstream << "X3 initializing..." << std::endl;
+    cout << "X3 initializing..." << endl;
     cam_func_ret = manifold_cam_init(GETBUFFER_MODE | TRANSFER_MODE);
     if (cam_func_ret == -1) {
-        logstream << "X3 init error" << std::endl;
+        cout << "X3 init error" << endl;
         return -1;
     }
     else {
-        logstream << "X3 inited" << std::endl;
+        cout << "X3 inited" << endl;
     }
     
-    logstream << "X3 reading first frame..." << std::endl;
+    cout << "X3 reading first frame..." << endl;
     cam_func_ret = manifold_cam_read(imgbuf, &cam_nframe, CAM_BLOCK);
     if (cam_func_ret == -1) {
-        logstream << "X3 first frame error" << std::endl;
-        return -1;
+        cout << "X3 first frame error" << endl;
+        cam_is_on = false;
     }
     else {
-        logstream << "X3 first frame read" << std::endl;
+        cout << "X3 first frame read" << endl;
         cam_is_on = true;
     }
 
@@ -87,7 +86,7 @@ int main() {
         while (cam_is_on) {
             tcp::socket socket(io_service);
             
-            logstream << "start accept..." << std::endl;
+            cout << "start accept..." << endl;
             accepted = false;
             while(!accepted && cam_is_on) {
                 io_service.reset();
@@ -110,7 +109,7 @@ int main() {
             }
 
             if (accepted) {
-                logstream << "accepted from " << socket.remote_endpoint() << " @ " << make_daytime_string() << std::endl;
+                cout << "accepted from " << socket.remote_endpoint() << " @ " << make_daytime_string() << endl;
             }
             
 
@@ -152,34 +151,34 @@ int main() {
 
                     // logout every frame for first 1 sec, every sec for first 1 minutes, and every minute 
                     if (frame_cnt<30 || (frame_cnt<1800 && frame_cnt%30==0) || frame_cnt%1800==0) {
-                        logstream << boost::format("#%d frame sent %d bytes") % frame_cnt % cnt << std::endl;
+                        cout << boost::format("#%d frame sent %d bytes") % frame_cnt % cnt << std::endl;
                     }
                     
                     if (!cnt) {
-                        logstream << "remote closed" << std::endl;
+                        cout << "remote closed" << endl;
                         socket.close();
                         break;
                     }
                 } catch (std::exception& e) {
-                    logstream << "ERROR in while(1):" << std::endl;
-                    logstream << e.what() << std::endl;
+                    cerr << "ERROR in while(1):" << endl;
+                    cerr << e.what() << endl;
                     break;
                 }
             }
-            logstream << "exit write loop..." << std::endl;
+            cout << "exit write loop..." << endl;
         }
-        logstream << "exit accept loop..." << std::endl;
+        cout << "exit accept loop..." << endl;
     } catch (std::exception& e) {
-        logstream << "ERROR outside while(1):" << std::endl;
-        logstream << e.what() << std::endl;
+        cerr << "ERROR outside while(1):" << endl;
+        cerr << e.what() << endl;
     }
 
-    logstream << "exit x3..." << std::endl;
+    cout << "exit x3..." << std::endl;
     while (!manifold_cam_exit()) {
-        logstream << "X3 failed to exit, retrying..." << std::endl;
+        cout << "X3 failed to exit, retrying..." << endl;
         sleep(1);
     }
-    logstream << "X3 exit" << std::endl;
+    cout << "X3 exit" << endl;
 
     return 0;
 }
